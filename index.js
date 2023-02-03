1b3040b5e77571e591321e74a71b1919cbab44a9
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-03 12:07:20 -0800

    completed lab with correction of appending to DOM

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ function submitData(nameVal, emailVal) {
 
     const call = fetch("http://localhost:3000/users", configurationObject)
     .then(resp => resp.json())
-    .then(obj => console.log(obj)).catch(error =>{
-        document.body.innerHTML = error.message;
+    .then(obj => {
+        const p = document.createElement('p');
+        p.textContent = obj.id;
+        document.body.append(p);
+    })
+        .catch(error =>{
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        document.body.append(p);
     })
 
     return call;
