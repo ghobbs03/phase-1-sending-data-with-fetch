ae0b2de27b8a7c55569b08da6d9e2afa266ff5bb
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-03 11:43:07 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+  
+function submitData(nameVal, emailVal) {
+    const formData = {
+        name: nameVal,
+        email: emailVal
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    const call = fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(obj => console.log(obj)).catch(error =>{
+        document.body.innerHTML = error.message;
+    })
+
+    return call;
+}
